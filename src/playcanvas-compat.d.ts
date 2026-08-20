5922a2572b234f2c38e3f3173342325d6db70d94
xash-mind <sashreeksagar5@gmail.com>
2026-08-20 22:22:58 +0530

    Keep visibility typings compatible with PlayCanvas Vec3

--- src/playcanvas-compat.d.ts
+++ src/playcanvas-compat.d.ts
@@ -1,14 +1,6 @@
 import 'playcanvas';
 
 declare module 'playcanvas' {
-  class Vec3 {
-    constructor(x?: number, y?: number, z?: number);
-    x: number;
-    y: number;
-    z: number;
-    set(x: number, y: number, z: number): Vec3;
-  }
-
   class BoundingSphere {
     constructor(center?: Vec3, radius?: number);
     readonly center: Vec3;
