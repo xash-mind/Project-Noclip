3637d177d2e437af02874b64b7c181e3a363b653
xash-mind <sashreeksagar5@gmail.com>
2026-08-21 01:59:11 +0530

    Restore frustum-only PlayCanvas compatibility

--- src/playcanvas-compat.d.ts
+++ src/playcanvas-compat.d.ts
@@ -1,18 +1,6 @@
 import 'playcanvas';
 
 declare module 'playcanvas' {
-  interface BoundingSphere {
-    center: { set(x: number, y: number, z: number): void };
-    radius: number;
-  }
-
-  interface Frustum {
-    containsSphere(sphere: BoundingSphere): number;
-  }
-
-  const Vec3: { new (): unknown };
-  const BoundingSphere: { new (center?: unknown, radius?: number): BoundingSphere };
-
   interface StandardMaterial {
     clone(): StandardMaterial;
   }
